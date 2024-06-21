a305b52971709df30c2515b9d9223c37d773af97
amageton23 <111858381+stevethefourth@users.noreply.github.com>
2024-06-21 15:10:51 +1000

    Script_created

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void PanCamera(){
+        if(Input.GetMouseButtonDown(0)){
+            
+        }
+    }
+}
