19ffac3677485548b3ffecf06735107b4885cd09
amageton23 <111858381+stevethefourth@users.noreply.github.com>
2024-07-04 05:00:05 +1000

    Toothless Animation stop and start + triggers
    Toothless animation now starts walking on start and will go to idle when reaching the gameObject stop.

--- Assets/Scripts/Movable.cs
+++ Assets/Scripts/Movable.cs
@@ -6,17 +6,30 @@ public class Movable : MonoBehaviour
 {
     private float move = 0.5f;
     private bool isBlocked = false;
-    
+   
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("Walking_start");
+    
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(Vector2.left*move*Time.deltaTime);
+        if(isBlocked == false){
+            transform.Translate(Vector2.right*move*Time.deltaTime);
+        }
+        
     }
+
+
+    private void OnTriggerEnter2D(Collider2D other){
+        animator.SetTrigger("Idle_start");
+        isBlocked = true;
+    }
+
 }
